a4eef6fd1b9eaf6510d7ffb3677bbf187d096b29
Alex <alex@Alexs-MacBook-Pro.local>
2019-03-21 14:47:49 -0600

    updated config again

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-"use strict";
-exports.API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
+};
